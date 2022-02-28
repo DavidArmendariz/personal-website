55b963a7c182065701c61dd75a4e9cfa8e6b07b8
David Armendariz <david.adrian.a.p@gmail.com>
2022-02-27 21:39:35 -0500

    Added gsap animations

--- app-components/SocialMedia/SocialMedia.tsx
+++ app-components/SocialMedia/SocialMedia.tsx
@@ -1,12 +1,14 @@
 import Link from 'next/link';
 import { SOCIAL_MEDIA_LINKS } from 'app-static';
 
-const SocialMedia: React.FC = () => {
+const SocialMedia: React.FC<{ setRefs?: (ref: any) => void }> = ({
+  setRefs,
+}) => {
   return (
     <>
       {SOCIAL_MEDIA_LINKS.map(({ url, Icon }) => (
         <Link key={url} href={url}>
-          <a target="_blank">
+          <a ref={setRefs} target="_blank">
             <Icon sx={{ m: 1 }} />
           </a>
         </Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,41 @@
 import Typography from '@mui/material/Typography';
+import gsap from 'gsap';
 import { PageHead, SocialMedia } from 'app-components';
+import { useArrayRef } from 'app-hooks';
+import { useIsomorphicLayoutEffect } from 'app-hooks/isomorphic-layout';
 
 const Home = () => {
+  const [refs, setRefs] = useArrayRef();
+  const [socialMediaRefs, setSocialMediaRefs] = useArrayRef();
+
+  const stagger = (currentRef: any) => {
+    gsap.from(currentRef, {
+      duration: 0.5,
+      scale: 0.5,
+      opacity: 0,
+      delay: 0.5,
+      stagger: 0.5,
+      ease: 'power4',
+    });
+  };
+
+  useIsomorphicLayoutEffect(() => {
+    stagger(refs.current);
+    stagger(socialMediaRefs.current);
+  });
+
   return (
     <>
       <PageHead title={'David Armendáriz | About me'} />
-      <Typography variant="h1">David Armendáriz</Typography>
-      <Typography variant="h2">Software Developer / Mathematician</Typography>
-      <SocialMedia />
-      <hr />
-      <Typography paragraph>
+      <Typography ref={setRefs} variant="h1">
+        David Armendáriz
+      </Typography>
+      <Typography ref={setRefs} variant="h2">
+        Software Developer / Mathematician
+      </Typography>
+      <SocialMedia setRefs={setSocialMediaRefs} />
+      <hr ref={setRefs} />
+      <Typography ref={setRefs} paragraph>
         I am a mathematician who transitioned to the Software Development
         industry. I am always learning new things and teaching them whenever I
         can. I also have a strong analytical and mathematical background.
