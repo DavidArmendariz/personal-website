675a0c38c5e2474ece44495a9432dea1218c4bd2
David Armendariz <david.adrian.a.p@gmail.com>
2024-01-08 01:22:36 -0500

    Made the header a client componenT

---
+++ app/header.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import AppStack from '@/base-components/AppStack';
+import { useTheme } from '@mui/material/styles';
+
+import AppText from '@/base-components/AppText';
+import SocialMedia from '@/components/SocialMedia';
+import { useMediaQuery } from '@mui/material';
+import Image from 'next/image';
+
+const Header = () => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('md'));
+
+  return (
+    <AppStack
+      direction={matches ? 'row' : 'column'}
+      alignItems="center"
+      spacing={3}
+    >
+      <Image
+        src="/photo.jpg"
+        alt="Profile picture from David"
+        width={200}
+        height={200}
+        priority={false}
+      />
+      <AppStack id="personal-info">
+        <AppText variant="h1">David Armendáriz</AppText>
+        <AppText variant="h2">Software Developer / Mathematician</AppText>
+        <SocialMedia />
+      </AppStack>
+    </AppStack>
+  );
+};
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import AppStack from '@/base-components/AppStack';
 import AppText from '@/base-components/AppText';
-import SocialMedia from '@/components/SocialMedia';
 import { Metadata } from 'next';
-import Image from 'next/image';
+import Header from './header';
 
 export const metadata: Metadata = {
   title: 'David Armendáriz - Software Developer / Mathematician',
@@ -12,20 +11,7 @@ export const metadata: Metadata = {
 const Home = () => {
   return (
     <AppStack spacing={2}>
-      <AppStack alignItems="center">
-        <Image
-          src="/photo.jpg"
-          alt="Profile picture from David"
-          width={200}
-          height={200}
-          priority={false}
-        />
-        <AppStack id="personal-info">
-          <AppText variant="h1">David Armendáriz</AppText>
-          <AppText variant="h2">Software Developer / Mathematician</AppText>
-          <SocialMedia />
-        </AppStack>
-      </AppStack>
+      <Header />
       <AppText component="p">
         I am a mathematician who transitioned to the Software Development
         industry. I am always learning new things and teaching them whenever I
