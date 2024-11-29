2db3a3ec7965266ced7f0fe8902208f1c2e7c449
David Armendariz <david.adrian.a.p@gmail.com>
2024-11-29 11:13:09 -0500

    Changes headline

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,14 @@ const Home = () => {
     <AppStack spacing={2}>
       <Header />
       <AppText component="p">
-        I am a mathematician who transitioned to the Software Development
-        industry. I am always learning new things and teaching them whenever I
-        can. I also have a strong analytical and mathematical background.
+        I am a mathematician and a frontend-focused full-stack software
+        engineer. I am always learning new things and spreading knowledge. My
+        main skill is to learn fast and to have a strong <b>analytical</b> and
+        <b>mathematical</b> mindset. I am open-minded, adaptable, a{' '}
+        <b>critical thinker</b>, a problem-solving lover and a{' '}
+        <b>clean-code obsessed</b>. Currently learning more about cybersecurity,
+        focusing on secure software development to build resilient systems while
+        ensuring strong security practices
       </AppText>
     </AppStack>
   );
